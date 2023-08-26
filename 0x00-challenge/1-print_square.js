c8a8a663b3ee04922df37ef1e62aa8386429d2b4
pytbyte <heretolearn1@gmail.com>
2023-08-27 00:26:51 +0300

    Debuging challange js edit

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,24 +1,27 @@
 #!/usr/bin/node
 /*
     Print a square with the character #
-    
-    The size of the square must be the first argument 
+
+    The size of the square must be the first argument
     of the program.
 */
 
+const process = require('process');
 
-if (process.argv.length <= 2) {
+if (process.argv.length !== 3) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
+    process.exit(1);
 }
 
-size = parseInt(process.argv[2], 16)
+const size = parseInt(process.argv[2]);
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
-        process.stdout.write("#");
-    }
-    process.stdout.write("\n");
+if (isNaN(size) || size <= 0) {
+    process.stderr.write("Invalid size argument\n");
+    process.exit(1);
+}
+
+for (let i = 0; i < size; i++) {
+    process.stdout.write("#".repeat(size) + "\n");
 }
